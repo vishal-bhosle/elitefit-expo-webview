b4e5c31a01eb2e339ca3e737eebf24e1115c9742
Vishal <zerocoolvishal@gmail.com>
2025-01-18 01:35:18 +0530

    Update StatusBar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout() {
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
-      <StatusBar style="auto" />
+      <StatusBar backgroundColor="black" style="light" translucent={false}/>
     </ThemeProvider>
   );
 }
